5e70c3e89b35478215dcc6ee03d21835de80aea9
Madhav Prajapati <madhavku248@gmail.com>
2024-08-07 17:13:23 +0530

    debug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import "./App.css";
 import Home from "./pages/home/Home";
 import LiveChat from "./pages/livechat/LiveChat";
 import Post from "./pages/posts/Post";
-import SingleChat from "./pages/SingleChat/Home"
-import {BrowserRouter, Route,Routes } from "react-router-dom";
+import SingleChat from "./pages/SingleChat/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/authantication/Login";
 import SignUp from "./pages/authantication/SignUp";
 // import Test from "./Component/Test";
@@ -11,28 +12,24 @@ import Profile from "./pages/Profile/Profile";
 import ChatProvider from "./context.js/chatContext";
 
 function App() {
-
-
-
   return (
     <>
       <BrowserRouter>
         <ChatProvider>
           <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-            <Route path="/" element={<LiveChat/>}/>
-            <Route path="/chat" element={<SingleChat/>}/>
-            <Route path="/playground" element={<Home/>}/>
-            <Route path="/groupchat" element={<Home/>}/>
-            <Route path="/post" element={<Post/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/" element={<LiveChat />} />
+            <Route path="/chat" element={<SingleChat />} />
+            <Route path="/playground" element={<Home />} />
+            <Route path="/groupchat" element={<Home />} />
+            <Route path="/post" element={<Post />} />
             {/* <Route path="/test" element={<Test/>}/> */}
-            <Route path="/Profile" element={<Profile/>}/>
+            <Route path="/Profile" element={<Profile />} />
             {/* <Route path="/landing" element={<LandingPage/>}/> */}
           </Routes>
         </ChatProvider>
       </BrowserRouter>
-
     </>
   );
 }
--- src/pages/home/UserList.js
+++ src/pages/home/UserList.js
@@ -7,73 +7,73 @@ export default function UserList() {
   const [userList, setUserList] = useState([]);
   const [myGroups, setMyGroups] = useState([]);
 
-  const getAllUser = async()=>{
-    const res = await get('/user/all-users');
+  const getAllUser = async () => {
+    const res = await get("/user/all-users");
     console.log(res.data);
     setUserList(res.data);
-  }
+  };
 
-  const getMyAllGroups = async()=>{
-    const res = await get('/chat/allgroup');
-    console.log(res)
+  const getMyAllGroups = async () => {
+    const res = await get("/chat/allgroup");
+    console.log(res);
     setMyGroups(res?.data);
-  }
+  };
 
   const createNewChat = async (id) => {
-    const res = await post('/chat', {userId : id});
+    const res = await post("/chat", { userId: id });
     console.log(res);
-    if(res.statusCode === 200) {
+    if (res.statusCode === 200) {
       setSelectedChat(res?.data._id);
     }
-  }
+  };
 
   const getSelectedChat = async (id) => {
-    console.log('my selected chat', id);
-      setSelectedChat(id);
-  }
-
-
-
+    console.log("my selected chat", id);
+    setSelectedChat(id);
+  };
 
   useEffect(() => {
     getMyAllGroups();
   }, []);
   return (
     // <div className="flex">
-      <div class="md:block h-screen hidden  w-1/6 flex-col overflow-y-auto left-2 border-4 border-gray-100 rounded-xl shadow-lg bg-white">
-        {/* <!-- search compt --> */}
-        <div class="py-4 px-2">
-          <input
-            type="text"
-            placeholder="search chatting"
-            class="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
-          />
-        </div>
-        {/* <!-- end search compt --> */}
-        {/* <!-- user list --> */}
-        {
-          myGroups?.map((group)=>(
-          <div class="flex flex-row py-4 px-2 justify-center items-center" role="button" onClick={()=>{getSelectedChat(group._id)}}>
-            <div class="w-1/4 mr-4">
-              <img
-                src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
-                class="object-cover h-12 w-12 rounded-full"
-                alt=""
-              />
-            </div>
-            <div class="w-full">
-              <div class="text-sm font-bold">{group?.chatName}</div>
-              <span class="text-xs text-gray-400 font-normal">
-                Pick me at 9:00 Am
-              </span>
-            </div>
-          </div>
-          ))
-        }
-      
-
-        {/* <!-- end user list --> */}
+    <div class="md:block h-screen hidden  w-1/6 flex-col overflow-y-auto left-2 border-4 border-gray-100 rounded-xl shadow-lg bg-white">
+      {/* <!-- search compt --> */}
+      <div class="py-4 px-2">
+        <input
+          type="text"
+          placeholder="search chatting"
+          class="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
+        />
       </div>
+      {/* <!-- end search compt --> */}
+      {/* <!-- user list --> */}
+      {myGroups?.map((group) => (
+        <div
+          class="flex flex-row py-4 px-2 justify-center items-center"
+          role="button"
+          onClick={() => {
+            getSelectedChat(group._id);
+          }}
+        >
+          <div class="w-1/4 mr-4">
+            <img
+              src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+              class="object-cover h-12 w-12 rounded-full"
+              alt=""
+            />
+          </div>
+          <div class="w-full">
+            <div class="text-sm font-bold">{group?.chatName}</div>
+            <span class="text-xs text-gray-400 font-normal">
+              Pick me at 9:00 Am
+            </span>
+          </div>
+        </div>
+      ))}
+
+      {/* <!-- end user list --> */}
+    </div>
     // </div>
   );
 }
